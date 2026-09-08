3fd1cbe4d7441394fed36c9de4df6caa323e4a5b
DuongNTT <326459632+duongngo705-droid@users.noreply.github.com>
2026-09-08 18:13:26 +0700

    Complete Day 1 Java exercises

---
+++ src/Excercise01.java
@@ -0,0 +1,9 @@
+public class Excercise01 {
+    public static void main(String[] arg) {
+        System.out.println("==== MY PROFILE =====");
+        System.out.println("Name: Duong");
+        System.out.println("Job: Manual Tester");
+        System.out.println("Goal: Automation Tester");
+        System.out.println("Language: Java");
+    }
+}
---
+++ src/LoginTest.java
@@ -0,0 +1,10 @@
+public class LoginTest {
+    public static void main(String[] args) {
+        System.out.println("==== LOGIN TEST ====");
+        System.out.println("Step 1: Open website");
+        System.out.println("Step 2: Enter username");
+        System.out.println("Step 3: Enter password");
+        System.out.println("Step 4: Click login");
+        System.out.println("Expected Result: Login Successfully");
+    }
+}
